7fa806edeb1d427ceaec82c4e36d8f3f9b09bf27
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-05-06 03:18:10 +0300

    test

--- env.sh
+++ env.sh
@@ -1,8 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-export HBNB_MYSQL_USER=hbnb_dev
-export HBNB_MYSQL_PWD=hbnb_dev_pwd
-export HBNB_MYSQL_HOST=localhost
-export HBNB_MYSQL_DB=hbnb_dev_db
-export HBNB_TYPE_STORAGE=db
-export HBNB_API_PORT=5000
+# Set environment variables
+export HBNB_MYSQL_USER="hbnb_dev"
+export HBNB_MYSQL_PWD="hbnb_dev_pwd"
+export HBNB_MYSQL_HOST="localhost"
+export HBNB_MYSQL_DB="hbnb_dev_db"
+export HBNB_TYPE_STORAGE="db"
+
+echo "Environment variables set:"
+echo "HBNB_MYSQL_USER=$HBNB_MYSQL_USER"
+echo "HBNB_MYSQL_PWD=$HBNB_MYSQL_PWD"
+echo "HBNB_MYSQL_HOST=$HBNB_MYSQL_HOST"
+echo "HBNB_MYSQL_DB=$HBNB_MYSQL_DB"
+echo "HBNB_TYPE_STORAGE=$HBNB_TYPE_STORAGE"
