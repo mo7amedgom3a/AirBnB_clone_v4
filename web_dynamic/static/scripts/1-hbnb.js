739102cac46764f572671fdf346c8cd70a91ae2f
mo7amedgom3a <mo7amed.gom3a.7moda@gmail.com>
2024-05-06 03:54:18 +0300

    test

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    const amenities = {};
+    $('input[type="checkbox"]').on('change', function () {
+        if (this.checked) {
+            amenities[$(this).data('id')] = $(this).data('name');
+        } else {
+            delete amenities[$(this).data('id')];
+        }
+        if (Object.values(amenities).length > 0) {
+            $('.amenities h4').text(Object.values(amenities).join(', '));
+        } else {
+            $('.amenities h4').html('&nbsp;');
+        }
+    });
+});
